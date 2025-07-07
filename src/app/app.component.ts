4e165ac25855703c546c1fe5ac38207ba1c7695f
Abdullah Razzaki <abdullahrazzaki@gmail.com>
2025-07-07 17:21:37 +0500

    Adds scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, ScrollToTopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
